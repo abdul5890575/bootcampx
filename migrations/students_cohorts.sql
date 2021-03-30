9338336b6c4538fbe6b8ceea2ea134b359a7cfeb
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-30 17:38:16 +0000

    populating teachers/request

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -5,7 +5,7 @@ CREATE TABLE cohorts (
     end_date DATE
 );
 
-CREATE TABLE students (
+CREATE TABLE student (
     id SERIAL PRIMARY KEY NOT NULL, 
     name VARCHAR(225) NOT NULL,
     email VARCHAR(255),
