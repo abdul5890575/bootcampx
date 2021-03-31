77c6f98a12c53a51afd9863924a94a75a51bf4b6
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-31 23:36:16 +0000

    connect sql with backend

--- 4_queries/name_of_teacher.sql
+++ 4_queries/name_of_teacher.sql
@@ -5,15 +5,3 @@ JOIN assistance_requests ON student_id = students.id
 JOIN teachers ON teacher_id = teachers.id
 WHERE cohorts.name = 'JUL02'
 ORDER BY teachers.name
-
-
-
-
--- //SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
--- FROM teachers
--- JOIN assistance_requests ON teacher_id = teachers.id
--- JOIN students ON student_id = students.id
--- JOIN cohorts ON cohort_id = cohorts.id
--- WHERE cohorts.name = 'JUL02'
--- ORDER BY teacher;
-
