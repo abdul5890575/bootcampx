d0f1070077403b4696fa806c21afd74a98f3e810
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-04-01 00:32:36 +0000

    parametized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,6 +6,8 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+
 //Without args
 // pool.query(`
 // SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -24,9 +26,9 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`, [ process.argv[2] , process.argv[3] || 5])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
